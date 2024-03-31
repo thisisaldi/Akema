7f99120fa737cbdc0dc52dec0d1fbcee6b69c220
thisisaldi <m.naufaldi.aldi@gmail.com>
2024-03-31 13:30:33 +0700

    Updated Home Activity: Add PresenceFragment Popup and BottomNavigationView

--- app/src/main/java/com/kari/akema/HomeActivity.kt
+++ app/src/main/java/com/kari/akema/HomeActivity.kt
@@ -1,11 +1,35 @@
 package com.kari.akema
 
+import PresenceFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
+@Suppress("DEPRECATION")
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        popupPresenceButtonInitialize()
+        bottomNavigationViewInitialize()
+    }
+
+    private fun bottomNavigationViewInitialize() {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_UNLABELED
+        bottomNavigationView.itemIconTintList = null
+        bottomNavigationView.backgroundTintList = null
+    }
+
+    private fun popupPresenceButtonInitialize() {
+        val presenceButton: Button = findViewById(R.id.presence_button)
+        presenceButton.setOnClickListener {
+            val bottomSheetFragment = PresenceFragment()
+            bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+        }
     }
 }
---
+++ app/src/main/java/com/kari/akema/PresenceFragment.kt
@@ -0,0 +1,15 @@
+// BottomSheetFragment.kt
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.kari.akema.R
+
+class PresenceFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_presence, container, false)
+    }
+}
