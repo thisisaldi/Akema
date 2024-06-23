5941ba05fb8e780df09f9635e9162c2c8fe4a49f
Irsyad <azcat01@yahoo.co.id>
2024-06-23 17:52:00 +0700

    fix changes

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { env } = require("./server.config");
 
-const uri = env === 'prod'? process.env.ATLAS_URI : 'mongodb://localhost:27017/akema';
+const uri = env.trim() == 'prod' ? process.env.ATLAS_URI : 'mongodb://localhost:27017/akema';
     
 module.exports = {
   uri
